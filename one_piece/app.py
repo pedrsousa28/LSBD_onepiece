c6e5203a0056a4b6ddea8aa3b4c0570a5dc81c09
unknown <pedro.sousa@lsbd.ufc.br>
2023-08-16 14:27:35 -0300

    atualização

--- one_piece/app.py
+++ one_piece/app.py
@@ -1,87 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_mysqldb import MySQL
 
+app = Flask (__name__)
 
-
-app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'onepiece' 
-app.config['MYSQL_USER'] = "LUFFY"
+app.config["MYSQL_USER"]="root"
+app.config["MYSQL_PASSWORD"]=""
+app.config["MYSQL_DB"]="onepiece"
+app.config["MYSQL_HOST"]="localhost"
 
 mysql = MySQL(app)
 
-
 @app.route('/')
-def Index():   
-    cur = mysql.connection.cursor()
-    cur.execute ("SELECT * FROM personagens")
-    data=cur.fetchall()
-    cur.close()
+def Index():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM perso")
+    data=cursor.fetchall()
+    cursor.close()
+    return render_template ( 'index.html', perso=data )
 
 
 
-
-    return render_template('index2.html', personagens=data )
-
-
-
-@app.route('/insert', methods = ['POST'])
-def insert():
-
+@app.route('/create', methods = ['POST'])
+def create():
     if request.method == "POST":
-        flash("PERSONAGEM INSERIDO COM SUCESSO")
-        nome = request.form['nome']
-        idade = request.form['idade']
-        lugar = request.form['lugar_de_origem']
-        descricao = request.form['descricao']
-        cursor= mysql.connection.cursor()
-        cursor.execute("INSERT INTO personagens (nome, idade, lugar_de_origem, descricao) VALUES (%s, %s, %s, %s)", (nome, idade, lugar, descricao))
-        mysql.connection.commit()
-        return redirect(url_for('Index'))
+     nome=request.form['nome']
+     idade = request.form['idade']
+     lugar = request.form['lugar']
+     descricao = request.form['descricao']
+     cursor = mysql.connector.connect()
+     cursor.execute("INSERT INTO perso (nome, idade, lugar, descricao) VALUES (%s, %s, %s, %s)", (nome, idade, lugar, descricao))
+     mysql.connector.commit()
+     return redirect(url_for('Index'))
 
-
-
-
-@app.route('/delete/<string:nome>', methods = ['GET'])
-def delete(nome):
-    flash("Record Has Been Deleted Successfully")
-    cursor=mysql.connection.cursor()
-    cursor.execute("DELETE FROM personagens WHERE nome=%s", (nome,))
-    mysql.connection.commit()
-    return redirect(url_for('Index'))
-
-
-
-
-
-@app.route('/update',methods=['POST','GET'])
-def update():
-
-    if request.method == 'POST':
-        nome = request.form['nome']
-        idade = request.form['idade']
-        lugar = request.form['lugar_de_origem']
-        descricao = request.form['descricao']
-        cursor = mysql.connection.cursor()
-        cursor.execute("""
-               UPDATE personagens
-               SET nome=%s, idade=%s, lugar_de_origem=%s, descricao=%s
-               WHERE id=%s
-            """, (nome, idade, lugar, descricao))
-        flash("Atualizado")
-        mysql.connection.commit()
-        return redirect(url_for('Index'))
+@app.route('/edit', methods=['POST,GET'])
+def edit():
+   if request.method == "POST":
+     nome=request.form['nome']
+     idade = request.form['idade']
+     lugar = request.form['lugar']
+     descricao = request.form['descricao']
+     cursor = mysql.connector.connect()
+     cursor.execute("UPDATE perso SET nome=%s, idade=%s, lugar=%s, descricao=%s", (nome, idade, lugar, descricao,))
+     mysql.connector.commit()
+     return redirect (url_for('Index'))
+   
 
 
 
 
 
-
-
-
-
-if __name__ == "__main__":
+   if __name__ == "__main__":
     app.run(debug=True)
