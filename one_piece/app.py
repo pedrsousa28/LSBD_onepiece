172047a3c6374a8b4d41d6ac0cfc76295da6d1bd
Pedro Sousa <pedro.sousa@lsbd.ufc.br>
2023-08-17 14:45:30 -0300

    criando páginas html, formulario de edição e criação de personagens, edição na pagina index, editando masi a parte de front-end, back vê depois

--- one_piece/app.py
+++ one_piece/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_mysqldb import MySQL 
-from flask import request
-from jinja2 import Environment, PackageLoader
-env = Environment (loader=PackageLoader('app', 'templates'))
 
 app = Flask (__name__)
 
@@ -24,10 +21,10 @@ def Index():
 
 
 
-@app.route('/', methods=['POST'])
+@app.route('/create', methods=['POST'])
 def create():
     if request.method == 'POST':
-      id = request.form['ID']
+      inde = request.form['ID']
       nome = request.form['name']
       idade = request.form['idade']
       lugar = request.form['lugar']
@@ -35,13 +32,13 @@ def create():
       cursor = mysql.connection.connect()
       cursor.execute("INSERT INTO personagens (nome, idade, lugar, descricao) VALUES (%s, %s, %s, %s)", (nome, idade, lugar, descricao))
       mysql.connection.commit()
-      return redirect(create.html)
+      return render_template('create.html')
     return ""
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
    if request.method == 'POST': 
-     id = request.form['ID']
+     inde = request.form['ID']
      nome = request.form['name']
      idade = request.form['idade']
      lugar = request.form['lugar']
@@ -49,7 +46,7 @@ def edit():
      cursor = mysql.connection.cursor()
      cursor.execute("UPDATE personagens SET nome=%s, idade=%s, lugar=%s, descricao=%s", (nome, idade, lugar, descricao,))
      mysql.connection.commit()
-     return ('edit.html')
+     return render_template('edit.html')
    return ""
    
 @app.route('/delete/<string:id>', methods=['GET'])
