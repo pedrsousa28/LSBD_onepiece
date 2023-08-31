c0ad1045b2c956656a57b433e17076ea1420cf86
Pedro Sousa <pedro.sousa@lsbd.ufc.br>
2023-08-31 16:47:13 -0300

    correção: mostrando valores na página edit, editando normalmente, futura adição da página do personagem

--- one_piece/app.py
+++ one_piece/app.py
@@ -12,57 +12,65 @@ mysql = MySQL(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
-def Index():
+def index():
     if request.method == 'GET':
-      Cursor = mysql.connection.Cursor()
-      Cursor.execute("SELECT * FROM personagens")
-      Data=Cursor.fetchall()
-      Cursor.close()
-    return render_template('index.html', perso=Data)
+      cursor = mysql.connection.cursor()
+      cursor.execute("SELECT * FROM personagens")
+      data=cursor.fetchall()
+      print (data)
+      cursor.close()
+    return render_template('index.html', perso=data)
 
 
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
-      Nome = request.form['nome']
-      Idade = request.form['idade']
-      Lugar = request.form['lugar']
-      Descricao = request.form['descricao']
-      Cursor = mysql.connection.Cursor()
-      Cursor.execute("INSERT INTO personagens (nome, idade, lugar, descricao) VALUES (%s, %s, %s, %s)", (Nome, Idade, Lugar, Descricao))
+      nome = request.form['nome']
+      idade = request.form['idade']
+      lugar = request.form['lugar']
+      descricao = request.form['descricao']
+      cursor = mysql.connection.cursor()
+      cursor.execute("INSERT INTO personagens (nome, idade, lugar, descricao) VALUES (%s, %s, %s, %s)", (nome, idade, lugar, descricao))
       mysql.connection.commit()
     return render_template('create.html', )
     
 
 @app.route('/edit/<int:id>', methods=['POST','GET'])
 def edit(id):
-   if request.method == 'POST': 
-     Nome = request.form['nome']
-     Idade = request.form['idade']
-     Lugar = request.form['lugar']
-     Descricao = request.form['descricao']
-     Cursor = mysql.connection.Cursor()
-     Cursor.execute("UPDATE personagens SET nome=%s, idade=%s, lugar=%s, descricao=%s WHERE id=%s", (Nome, Idade, Lugar, Descricao, id))
+   if request.method == 'POST':
+     nome = request.form['nome']
+     idade = request.form['idade']
+     lugar = request.form['lugar']
+     descricao = request.form['descricao']
+     cursor = mysql.connection.cursor()
+     cursor.execute("UPDATE personagens SET nome=%s, idade=%s, lugar=%s, descricao=%s WHERE id=%s", (nome, idade, lugar, descricao, id))
      mysql.connection.commit()
-   return render_template('edit.html')
+     return redirect (url_for('index'))
+   else:  # elif request.method == 'GET'   
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM personagens WHERE id=%s", (id,))
+    data=cursor.fetchall()
+    cursor.close() 
+    print(data)
+    return render_template('edit.html', perso=data[0])
 
 @app.route('/<nome>', methods=['POST', 'GET'])
 def personagens(nome):
    if request.method == 'GET':
-     Cursor = mysql.connection.Cursor()
-     Cursor.execute("SELECT * FROM personagens WHERE nome=%s", (nome))
-     Data=Cursor.fetchall()
-     Cursor.close()
-   return render_template('persona.html', perso=Data)
+     cursor = mysql.connection.cursor()
+     cursor.execute("SELECT * FROM personagens WHERE nome=%s", (nome,))
+     data=cursor.fetchall()
+     cursor.close()
+   return render_template('persona.html', perso=data)
    
    
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete(id):
-   Cursor = mysql.connection.Cursor()
-   Cursor.execute("DELETE FROM personagens WHERE id=%s", (id))
+   cursor = mysql.connection.cursor()
+   cursor.execute("DELETE FROM personagens WHERE id=%s", (id,))
    mysql.connection.commit()
-   return redirect (url_for('Index'))
+   return redirect (url_for('index'))
 
 
 
