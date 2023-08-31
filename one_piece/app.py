2e7e1bc19fec23842840c15abd940fa3acd88e5b
Pedro Sousa <pedro.sousa@lsbd.ufc.br>
2023-08-31 10:55:12 -0300

    correção: adição da rota para personagens e do css para o mesmo

--- one_piece/app.py
+++ one_piece/app.py
@@ -11,13 +11,14 @@ app.config["MYSQL_HOST"]="localhost"
 mysql = MySQL(app) 
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def Index():
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM personagens")
-    data=cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', perso=data)
+    if request.method == 'GET':
+      Cursor = mysql.connection.Cursor()
+      Cursor.execute("SELECT * FROM personagens")
+      Data=Cursor.fetchall()
+      Cursor.close()
+    return render_template('index.html', perso=Data)
 
 
 
@@ -45,11 +46,21 @@ def edit(id):
      Cursor.execute("UPDATE personagens SET nome=%s, idade=%s, lugar=%s, descricao=%s WHERE id=%s", (Nome, Idade, Lugar, Descricao, id))
      mysql.connection.commit()
    return render_template('edit.html')
+
+@app.route('/<int:id>', methods=['POST', 'GET'])
+def personagens(id):
+   if request.method == 'GET':
+     Cursor = mysql.connection.Cursor()
+     Cursor.execute("SELECT * FROM personagens")
+     Data=Cursor.fetchall()
+     Cursor.close()
+   return render_template('persona.html', perso=Data)
+   
    
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete(id):
-   cursor = mysql.connection.cursor()
-   cursor.execute("DELETE FROM personagens WHERE id=%s", (id))
+   Cursor = mysql.connection.Cursor()
+   Cursor.execute("DELETE FROM personagens WHERE id=%s", (id))
    mysql.connection.commit()
    return redirect (url_for('Index'))
 
